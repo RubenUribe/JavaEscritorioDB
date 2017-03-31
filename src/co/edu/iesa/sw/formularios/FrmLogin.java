208ad6170392614b32ee9e38f3873c2d0ea657b5
RubenUribe <uriberuben89@gmail.com>
2017-03-30 19:42:15 -0500

    Modificación de frmLogin para intergra el DaoUsuario

--- src/co/edu/iesa/sw/formularios/FrmLogin.java
+++ src/co/edu/iesa/sw/formularios/FrmLogin.java
@@ -1,5 +1,7 @@
 package co.edu.iesa.sw.formularios;
 
+import co.edu.iesa.sw.entidades.Usuario;
+import co.edu.iesa.sw.persistencia.DaoUsuario;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -14,7 +16,7 @@ public class FrmLogin extends JFrame implements ActionListener{
     private final JLabel lblLogo;
     private final JLabel lblUsuario;
     private final JLabel lblContrasena;
-    private final JTextField txtUsuario;
+    private final JTextField txtEmail;
     private final JPasswordField passContrasena;
     private final JButton btnIngresar;
     private final JButton btnCancelar;
@@ -43,8 +45,8 @@ public class FrmLogin extends JFrame implements ActionListener{
         //Intaciación o creación de los objetos del marco
         lblLogo = new JLabel(iconLogo);
         //lblLogo.setIcon(new ImageIcon(getClass().getResource("../imagenes/IconoLogin.png")));
-        lblUsuario = new JLabel("Usuario");
-        txtUsuario = new JTextField();
+        lblUsuario = new JLabel("Correo electrónico");
+        txtEmail = new JTextField();
         lblContrasena = new JLabel("Contraseña");
         passContrasena = new JPasswordField();
         btnCancelar = new JButton("",iconCancelar);
@@ -66,7 +68,7 @@ public class FrmLogin extends JFrame implements ActionListener{
         
 
         //texto de ayuda
-        txtUsuario.setToolTipText("Ingrese su usuario");
+        txtEmail.setToolTipText("Ingrese su usuario");
         passContrasena.setToolTipText("Ingrese su contraseña");
         btnCancelar.setToolTipText("Cancelar el ingreso al sistema");
         btnIngresar.setToolTipText("Ingresar al sistema");
@@ -85,7 +87,7 @@ public class FrmLogin extends JFrame implements ActionListener{
         //Adicionar objetos al marco
         add(lblLogo);
         add(lblUsuario);
-        add(txtUsuario);
+        add(txtEmail);
         add(lblContrasena);
         add(passContrasena);
         add(btnCancelar);
@@ -93,8 +95,8 @@ public class FrmLogin extends JFrame implements ActionListener{
         
         //Ubicación y tamaño de los objetos en el marco
         lblLogo.setBounds(65, 40, 128, 128);
-        lblUsuario.setBounds(40, 190, 50, 25);
-        txtUsuario.setBounds(40, 210, 180, 25);
+        lblUsuario.setBounds(40, 190, 110, 25);
+        txtEmail.setBounds(40, 210, 180, 25);
         lblContrasena.setBounds(40, 240, 80, 25);
         passContrasena.setBounds(40, 260, 180, 25);
         btnCancelar.setBounds(40, 300, 85, 30);
@@ -113,30 +115,36 @@ public class FrmLogin extends JFrame implements ActionListener{
          
          if (e.getSource()==btnIngresar) {
              
-             if(txtUsuario.getText().equals("uriberuben89@gmail.com") && passContrasena.getText().equals("123")){
-                 JOptionPane.showMessageDialog(null,"Bienvenido "+txtUsuario.getText(),"Mensaje",JOptionPane.INFORMATION_MESSAGE);
-                 dispose();
-                 frmPrincipal miPrincipal = new frmPrincipal();
-                 miPrincipal.setVisible(true);
+             String email = txtEmail.getText();
+             //Si campo correo vacio
+             if(email.isEmpty()){
+                 JOptionPane.showMessageDialog(null,"Debe ingresar un correo electrónico","Mensaje",JOptionPane.INFORMATION_MESSAGE);
+                 txtEmail.requestFocusInWindow();
+                 return;
              }
-             else{
-                if(txtUsuario.getText().equals("") || passContrasena.getPassword().equals("")){
-                JOptionPane.showMessageDialog(null,"Ingrese usuario y/o contraseña","Mensaje",JOptionPane.INFORMATION_MESSAGE);
-                //Limpiar campo de texto usuario
-                txtUsuario.setText("");
-                //Limpiar campo de texto contraseña
-                passContrasena.setText("");
-                txtUsuario.requestFocusInWindow();
-                }
-                else{
-                    JOptionPane.showMessageDialog(null,"usuario y/o contraseña incorrecta","Mensaje",JOptionPane.INFORMATION_MESSAGE);
-                     //Limpiar campo de texto usuario
-                    txtUsuario.setText("");
-                    //Limpiar campo de texto contraseña
-                    passContrasena.setText("");
-                    txtUsuario.requestFocusInWindow();
-                }   
-             } 
+             
+             String password = new String(passContrasena.getPassword());
+             //Si campo contraseña vacio
+             if(password.isEmpty()){
+                 JOptionPane.showMessageDialog(null,"Debe ingresar una contraseña","Mensaje",JOptionPane.INFORMATION_MESSAGE);
+                 passContrasena.requestFocusInWindow();
+                 return;
+             }
+  
+             Usuario miUsuario = DaoUsuario.getUsuario(email, password);
+             
+             if(miUsuario == null){
+                 JOptionPane.showMessageDialog(null,"correo o contraseña incorrecta","Mensaje",JOptionPane.INFORMATION_MESSAGE);
+                 //Limpiar campo contraseña
+                 passContrasena.setText("");
+                 passContrasena.requestFocusInWindow();
+                 return;
+             }
+ 
+             JOptionPane.showMessageDialog(null,"Bienvenido "+miUsuario.getNombre()+" "+miUsuario.getApellido());
+             dispose();
+             frmPrincipal miPrincipal = new frmPrincipal();
+             miPrincipal.setVisible(true);
          }     
     }
 }
--- src/co/edu/iesa/sw/persistencia/DaoUsuario.java
+++ src/co/edu/iesa/sw/persistencia/DaoUsuario.java
@@ -1,9 +1,47 @@
 package co.edu.iesa.sw.persistencia;
 
+import co.edu.iesa.sw.conexion.DBConexion;
+import co.edu.iesa.sw.entidades.Usuario;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  *
  * @author Rubén Darío Uribe Quejada
  */
+/* DAO: data acces objects*/
 public class DaoUsuario {
-    
+    public static Usuario getUsuario(String email, String password){
+        
+        DBConexion con = new DBConexion();
+        try {
+            PreparedStatement consulta = con.getConnection()
+                    .prepareStatement("SELECT * FROM usuarios WHERE email = ? AND password = ?");
+
+            consulta.setString(1, email);
+            consulta.setString(2, password);
+            ResultSet resultado = consulta.executeQuery();
+            
+            //El metodo next() devuelve true miestras existas mas registros
+            if(!resultado.next()){
+                return null;
+            }
+            Usuario miUsuario = new Usuario(
+                    resultado.getString("email"), 
+                    resultado.getString("nombre"), 
+                    resultado.getString("apellido"), 
+                    resultado.getString("password"), 
+                    resultado.getInt("rolId"));
+            return miUsuario;
+            
+        } catch (SQLException ex) {
+            //Logger.getLogger(DaoUsuario.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+ 
+    }
 }
